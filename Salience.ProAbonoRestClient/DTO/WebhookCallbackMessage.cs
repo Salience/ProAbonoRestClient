41ccc562226f3240e4028be94b68e0be6b129910
Julien Patte <julien.patte@gmail.com>
2016-10-08 15:56:39 +0200

    added DTO for invoice debits in webhook notifications

--- Salience.ProAbonoRestClient/DTO/WebhookCallbackMessage.cs
+++ Salience.ProAbonoRestClient/DTO/WebhookCallbackMessage.cs
@@ -5,9 +5,9 @@ namespace ProAbono
     public class WebhookCallbackMessage
     {
         public string Code { get; set; }
-        public int Id { get; set; }
-        public int IdBusiness { get; set; }
-        public int IdWebhook { get; set; }
+        public long Id { get; set; }
+        public long IdBusiness { get; set; }
+        public long IdWebhook { get; set; }
         public string ReferenceSegment { get; set; }
         public string Currency { get; set; }
         public DateTime DateTrigger { get; set; }
@@ -16,17 +16,18 @@ namespace ProAbono
         public WebhookCallbackMessageCustomer CustomerBuyer { get; set; }
         public WebhookCallbackMessageOffer Offer { get; set; }
         public WebhookCallbackMessageSubscription Subscription { get; set; }
+        public WebhookCallbackMessageInvoiceDebit InvoiceDebit { get; set; }
     }
 
     public class WebhookCallbackMessageCustomer : CustomerInfo
     {
-        public int Id { get; set; }
+        public long Id { get; set; }
         public string ReferenceCustomer { get; set; }
     }
 
     public class WebhookCallbackMessageOffer
     {
-        public int Id { get; set; }
+        public long Id { get; set; }
         public string ReferenceOffer { get; set; }
         public bool IsVisible { get; set; }
         public string Name { get; set; }
@@ -37,12 +38,24 @@ namespace ProAbono
 
     public class WebhookCallbackMessageSubscription
     {
-        public int Id { get; set; }
-        public int IdSegment { get; set; }
+        public long Id { get; set; }
+        public long IdSegment { get; set; }
         public string StateSubscription { get; set; }
         public DateTime DateStart { get; set; }
         public int AmountRecurrence { get; set; }
         public int DurationRecurrence { get; set; }
         public TimeUnit UnitRecurrence { get; set; }
+    }
+
+    public class WebhookCallbackMessageInvoiceDebit
+    {
+        public long Id { get; set; }
+        public string FullNumber { get; set; }
+        public string StateInvoice { get; set; }
+        public DateTime? DateIssue { get; set; }
+        public DateTime? DatePayment { get; set; }
+        public string TypePayment { get; set; }
+        public int AmountSubtotal { get; set; }
+        public int AmountTotal { get; set; }
     }
 }
